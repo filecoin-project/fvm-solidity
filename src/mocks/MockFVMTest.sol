3a4a558799c44eabc5c1e99332d0a61f1bfa3511
Aashish <aashish.paliwal@live.com>
2026-03-01 22:17:53 +0530

    test(mocks): etch and copyStorage LOOKUP_DELEGATED_ADDRESS precompile

--- src/mocks/MockFVMTest.sol
+++ src/mocks/MockFVMTest.sol
@@ -3,7 +3,13 @@ pragma solidity ^0.8.30;
 
 import {Test} from "forge-std/Test.sol";
 
-import {CALL_ACTOR_BY_ADDRESS, CALL_ACTOR_BY_ID, GET_BEACON_RANDOMNESS, RESOLVE_ADDRESS} from "../FVMPrecompiles.sol";
+import {
+    CALL_ACTOR_BY_ADDRESS,
+    CALL_ACTOR_BY_ID,
+    GET_BEACON_RANDOMNESS,
+    RESOLVE_ADDRESS,
+    LOOKUP_DELEGATED_ADDRESS
+} from "../FVMPrecompiles.sol";
 import {FVMCallActorByAddress} from "./FVMCallActorByAddress.sol";
 import {FVMCallActorById} from "./FVMCallActorById.sol";
 import {FVMGetBeaconRandomness} from "./FVMGetBeaconRandomness.sol";
@@ -13,6 +19,7 @@ import {FVMActor} from "./FVMActor.sol";
 contract MockFVMTest is Test {
     FVMGetBeaconRandomness public constant RANDOMNESS_PRECOMPILE = FVMGetBeaconRandomness(GET_BEACON_RANDOMNESS);
     FVMActor public constant ACTOR_PRECOMPILE = FVMActor(RESOLVE_ADDRESS);
+    FVMActor public constant LOOKUP_DELEGATED_ADDRESS_PRECOMPILE = FVMActor(LOOKUP_DELEGATED_ADDRESS);
 
     function setUp() public virtual {
         vm.etch(CALL_ACTOR_BY_ADDRESS, address(new FVMCallActorByAddress()).code);
@@ -20,7 +27,11 @@ contract MockFVMTest is Test {
         vm.etch(GET_BEACON_RANDOMNESS, address(new FVMGetBeaconRandomness()).code);
 
         address deployed = address(new FVMActor());
+
         vm.etch(RESOLVE_ADDRESS, deployed.code);
+        vm.etch(LOOKUP_DELEGATED_ADDRESS, deployed.code);
+
         vm.copyStorage(deployed, RESOLVE_ADDRESS);
+        vm.copyStorage(deployed, LOOKUP_DELEGATED_ADDRESS);
     }
 }
