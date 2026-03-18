2bd481019b90293fd5f0fc8b8456932fa047089b
Aashish Paliwal <aashish.paliwal@live.com>
2026-03-18 06:42:15 +0530

    feat: add FRC-0042 method constants and CBOR codec (#13)
    * feat: add FRC-0042 hashed method constants
    
    * feat: add CBOR codec constant

--- src/FVMCodec.sol
+++ src/FVMCodec.sol
@@ -6,3 +6,6 @@ uint64 constant EMPTY_CODEC = 0;
 
 // @dev actor returned unencoded raw data
 uint64 constant RAW_CODEC = 0x55;
+
+// @dev CBOR encoded data (IPLD dag-cbor)
+uint64 constant CBOR_CODEC = 0x51;
--- src/FVMMethod.sol
+++ src/FVMMethod.sol
@@ -3,3 +3,11 @@ pragma solidity ^0.8.30;
 
 uint64 constant SEND = 0;
 uint64 constant CONSTRUCT = 1;
+
+// FRC-0042 hashed method numbers (https://github.com/filecoin-project/FIPs/blob/master/FRCs/frc-0042.md)
+uint64 constant AUTHENTICATE_MESSAGE = 2643134072;
+uint64 constant UNIVERSAL_RECEIVER_HOOK = 118350608;
+uint64 constant RECEIVE = 3726118371;
+uint64 constant MARKET_NOTIFY_DEAL = 4186741094;
+uint64 constant SECTOR_CONTENT_CHANGED = 2034386435;
+uint64 constant INVOKE_EVM = 3844450837;
